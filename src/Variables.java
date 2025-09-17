ced980da58411273139848f68497d7cb194b4296
dangdat1111 <dangdat234678@gmail.com>
2025-09-18 01:53:18 +0700

    add : Variable in Java

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int num = 3;
+
+        String name = "dat dep zai";
+
+        // Khi khai báo một biến float, bạn phải thêm ký tự f hoặc F vào cuối giá trị. Nếu không có ký tự này, trình biên dịch sẽ mặc định hiểu đó là kiểu double (kiểu số thực mặc định trong Java).
+        float marks = 6.5F;
+
+        System.out.println(num);
+        System.out.println(name);
+        System.out.println(marks);
+    }
+}
