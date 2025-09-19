766a7eb64394c1d7786ab180bbec75971f7787b1
dangdat1111 <dangdat234678@gmail.com>
2025-09-20 00:45:49 +0700

    commit : DataType

---
+++ src/DataType.java
@@ -0,0 +1,41 @@
+public class DataType {
+    public static void main(String[] args) {
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854L;
+
+        float f = 5.8F;
+        double d = 5.8;
+
+        char c = 'k';
+
+        boolean b = true;
+
+        //Literals trong Java là các giá trị cố định, được mã hóa trực tiếp vào chương trình. Chúng là những đại diện trực tiếp cho một giá trị, ví dụ như số 100, ký tự 'A', hoặc chuỗi "Hello".
+        // Integer Literals
+        int decimalVal = 100;
+        int octalVal = 0144; // 100
+        int hexVal = 0x64; // 100
+        long longVal = 10000000000L;
+
+        // Floating-Point Literals
+        float floatVal = 3.14f;
+        double doubleVal = 1.234E5; // 123400.0
+
+        // Character Literals
+        char charVal = 'J';
+        char unicodeChar = '\u004A'; // 'J'
+        char newLine = '\n';
+
+        // String Literals
+        String greeting = "Hello, Java!";
+
+        // Boolean Literals
+        boolean isTrue = true;
+
+        // Null Literal
+        String emptyString = null;
+
+    }
+}
