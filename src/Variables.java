ec5b62f0c90f1156bde2bec1f2ad2cb1fea17e85
dangdat1111 <dangdat234678@gmail.com>
2025-09-20 00:33:30 +0700

    commit : Variable

--- src/Variables.java
+++ src/Variables.java
@@ -7,8 +7,27 @@ public class Variables {
         // Khi khai báo một biến float, bạn phải thêm ký tự f hoặc F vào cuối giá trị. Nếu không có ký tự này, trình biên dịch sẽ mặc định hiểu đó là kiểu double (kiểu số thực mặc định trong Java).
         float marks = 6.5F;
 
-        System.out.println(num);
-        System.out.println(name);
-        System.out.println(marks);
+//        System.out.println(num); // 3
+//        System.out.println(name); // dat dep zai
+//        System.out.println(marks); // 6.5
+
+        // Truyền tham trị ( Pass by Value ) Biến gốc và biến trong phương thức là hai thực thể hoàn toàn độc lập trong bộ nhớ.
+        int a = 10 ;
+        changeValue(a);
+        System.out.println(a); //10
+
+        // Truyền Tham Chiếu (Pass by Reference) Biến gốc và biến trong phương thức đều trỏ đến cùng một đối tượng trong bộ nhớ.
+        int[] myArray = {1,2,3,4};
+        changeArray(myArray);
+        System.out.println(myArray[0]); // 99
     }
+
+    private static void changeValue(int number) {
+        number = 200;
+    }
+    private static void changeArray(int[] arr) {
+        arr[0] = 99;
+    }
+
+
 }
