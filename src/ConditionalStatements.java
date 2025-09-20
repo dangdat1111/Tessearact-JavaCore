2ffe26ae2fb30d79b250fe7503d4bf824f2cb329
dangdat1111 <dangdat234678@gmail.com>
2025-09-21 00:03:27 +0700

    commit: if-elseif-else

---
+++ src/ConditionalStatements.java
@@ -0,0 +1,8 @@
+public class ConditionalStatements {
+    public static void main(String[] args) {
+        int x = 8;
+
+        if (x>10 && x<=20) System.out.println("Hiii");
+        else System.out.println("Heloooo");
+    }
+}
