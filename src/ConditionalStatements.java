098a930fdd60021287c56c2aa41aeba6809a68a7
dangdat1111 <dangdat234678@gmail.com>
2025-09-21 00:08:37 +0700

    commit: ternary is a SHORTCUT for the If-Else

--- src/ConditionalStatements.java
+++ src/ConditionalStatements.java
@@ -4,5 +4,23 @@ public class ConditionalStatements {
 
         if (x>10 && x<=20) System.out.println("Hiii");
         else System.out.println("Heloooo");
+
+        // ternary is a SHORTCUT for the If-Else
+        // variable = (condition) ? value_if_true : value_if_false;
+
+        int n =4;
+        int result = 0;
+
+        //Not Ternary
+        if (n%2 == 0 )
+            result = 10;
+        else
+            result = 20;
+        System.out.println("Not Ternary "  + result);
+
+        // Ternary
+        result = n%2==0 ? 10 : 20;
+        System.out.println("Ternary " + result);
+
     }
 }
