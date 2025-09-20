4dc4b6fdc56b4e3a23259acb4c00aaa35862d553
dangdat1111 <dangdat234678@gmail.com>
2025-09-20 23:08:11 +0700

    Type Conversion and Casting

---
+++ src/TypeConversion.java
@@ -0,0 +1,10 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        byte b = 127;
+        int a = 256;
+
+        // convert
+        a = (int)b;
+        System.out.println(a);
+    }
+}
