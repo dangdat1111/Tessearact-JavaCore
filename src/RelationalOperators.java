3fb1bd05be9739e1452e4d0dad088609f2c59d84
dangdat1111 <dangdat234678@gmail.com>
2025-09-20 23:58:00 +0700

    commit: <, >, <=, >=, ==,!=, &&, ||

---
+++ src/RelationalOperators.java
@@ -0,0 +1,12 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 5;
+        double a = 8.8;
+        double b = 9.9;
+
+        boolean result = x != y || a >= b; // <,>,<=,>=,==,!=, &&, ||
+
+        System.out.println(result);
+    }
+}
