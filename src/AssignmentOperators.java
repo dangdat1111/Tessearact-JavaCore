f71256d75cd885039aa3b70813268099d0ed7bc5
dangdat1111 <dangdat234678@gmail.com>
2025-09-20 23:25:49 +0700

    Toán tử gán

---
+++ src/AssignmentOperators.java
@@ -0,0 +1,14 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int num1 = 7;
+        num1 += 2; // num1 = num1 + 2
+        num1++;
+        ++num1;
+
+
+        int num2 = 5;
+        int result = ++num2; // 6
+        int result = num2++; // 5
+        System.out.println(result);
+    }
+}
