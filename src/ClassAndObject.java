ba7f2ee4fbc27f70460c659089eb28ebe04d12c8
dangdat1111 <dangdat234678@gmail.com>
2025-09-26 20:27:27 +0700

    commit : Class and Object

--- src/ClassAndObject.java
+++ src/ClassAndObject.java
@@ -1,8 +1,14 @@
 
 class Calculator{
+    int a ;
+
     public int add (int num1, int num2){
         return num1 + num2;
     }
+    public void println(){
+        System.out.println("hiiiiiiii");
+    }
+
 }
 
 public class ClassAndObject {
@@ -12,6 +18,10 @@ public class ClassAndObject {
         int num1 = 4;
         int num2 = 5;
 
+        int number = 10;
+        calc.a =  number;
+        System.out.println(number);
+        calc.println();
         System.out.println(calc.add(num1, num2));
 
 
