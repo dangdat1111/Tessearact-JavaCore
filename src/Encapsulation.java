b88c29108683aee4470e2afb8902a59dc1f7febb
dangdat1111 <dangdat234678@gmail.com>
2025-09-27 23:13:50 +0700

    commit : Encapsulation

---
+++ src/Encapsulation.java
@@ -0,0 +1,23 @@
+class Human{
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Human(String name) {
+        this.name = name;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Human obj = new Human("datdepzai");
+        obj.setName("dat");
+        System.out.println(obj.getName());
+    }
+}
