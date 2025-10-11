dc139d6226f56039484e644b4ca0ff5b15058693
dangdat1111 <dangdat234678@gmail.com>
2025-10-11 16:19:58 +0700

    commit : Abtract

--- src/Abtract.java
+++ src/Abtract.java
@@ -1,5 +1,6 @@
 abstract class Car{
     public abstract void drive();
+    public abstract void fire();
     public void playMusic(){
         System.out.println("play Music");
     }
@@ -9,11 +10,17 @@ class VinFast extends Car{
     public void drive(){
         System.out.println("Drive");
     }
+
+    @Override
+    public void fire(){
+        System.out.println("fire");
+    }
 }
 public class Abtract {
     public static void main(String[] args) {
         Car obj = new VinFast();
         obj.drive();
         obj.playMusic();
+        obj.fire();
     }
 }
