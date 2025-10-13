5ef964accb19f4c1eb200c787362f175c9adf628
dangdat1111 <dangdat234678@gmail.com>
2025-10-13 14:03:45 +0700

    commit : class - class ===> Extends class - interface ===> Implements interface - interface ===> Extends

--- src/Interfaces.java
+++ src/Interfaces.java
@@ -1,28 +1,54 @@
 interface HanhDong{
-    abstract void diChuyen();
+    //Một interface chỉ chứa các phương thức trừu tượng
+    // (phương thức không có phần thân, tức là không có logic thực thi)
+    // và các hằng số (biến public static final).
+    void diChuyen();
     int TOC_DO_TOI_DA = 100;
 }
 
-class Oto implements HanhDong{
+interface DoXang{
+    void doXang();
+}
+
+class Oto implements HanhDong, DoXang{
     @Override
     public void diChuyen(){
         System.out.println(" O to runnnnn");
     }
+
+    @Override
+    public void doXang(){
+        System.out.println("o to do xang");
+    }
 }
 
-class MayBay implements HanhDong {
+class MayBay implements HanhDong, DoXang {
     @Override
     public void diChuyen() {
         System.out.println("Máy bay đang cất cánh lên bầu trời.");
     }
+
+    @Override
+    public void doXang(){
+        System.out.println("may bay do xang");
+    }
 }
+
+/*
+class - class ===> Extends
+class - interface ===> Implements
+interface - interface ===> Extends
+ */
 
 public class Interfaces {
     public static void main(String[] args) {
         Oto oto = new Oto();
         oto.diChuyen();
+        oto.doXang();
+        System.out.println(Oto.TOC_DO_TOI_DA);
 
         MayBay mayBay = new MayBay();
         mayBay.diChuyen();
+        mayBay.doXang();
     }
 }
