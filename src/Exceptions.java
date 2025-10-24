6f30b853b2ccab06f2a60563a2fcd4e10bb21449
dangdat1111 <dangdat234678@gmail.com>
2025-10-24 16:50:05 +0700

    commit : multi Catch

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -7,11 +7,17 @@ Error
 
 public class Exceptions {
     public static void main(String[] args) {
-        int i  = 0;
+        int i  = 1;
         int j = 0;
+
+        int nums[] = new int[5];
+
         try {
             j = 18/i;
-        }catch (Exception e){
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);
+        }
+        catch (ArithmeticException | ArrayIndexOutOfBoundsException e){
             System.out.println("Some thing went wrong " + e);
         }
 
