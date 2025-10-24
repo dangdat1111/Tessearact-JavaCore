6b495e65a45d19affccdfebad51720a2fcf1c1b2
dangdat1111 <dangdat234678@gmail.com>
2025-10-24 15:29:11 +0700

    commit : Function Interface - đánh dấu 1 interface chỉ có 1 method duy nhất

---
+++ src/FunctionInterface.java
@@ -0,0 +1,13 @@
+
+@FunctionalInterface // đánh dấu 1 interface chỉ có 1 method duy nhất
+interface AFI{
+    void show();
+    void run();
+
+}
+
+public class FunctionInterface {
+    public static void main(String[] args) {
+
+    }
+}
